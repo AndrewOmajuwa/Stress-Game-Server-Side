746891afb902f3a35afee5be54afef0cd23c16bd
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-08-01 02:04:29 +0200

    modified package.json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const user = require('./user/router')
 
 const app = express()
 const jsonParser = bodyParser.json()
